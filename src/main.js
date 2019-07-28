e2941e27abc492563e91f3c92b259ec3f7c3f76f
1546774884 <1546774884@qq.com>
2019-07-28 15:26:41 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuex from 'vuex'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
